01d7f77e26cd834d2e90a0efc7a57b3e907f9a8c
Halide Bey <halidebey@Halide-Bey-MBP.local>
2019-07-15 16:55:14 +0100

    renamed setup

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='test',
+    name='test-package',
     version='0.1.0',
-    packages=find_packages(include=['test', 'test.*'])
+    packages=find_packages(include=['test'])
 )
